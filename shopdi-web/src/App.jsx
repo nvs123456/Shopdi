41fcec08b3e610cf203b309370caf847f3a47cc2
TN <thinhvnu2022@gmail.com>
2024-10-25 21:04:58 +0700

    change the file tree system and add Order-details

--- shopdi-web/src/App.jsx
+++ shopdi-web/src/App.jsx
@@ -1,10 +1,15 @@
 import Footer from './components/Footer/Footer'
 import AppRouter from './routes/AppRouter'
+import Navigation from './components/Navigation/Navigation'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
 
   return (
     <div className='flex flex-col min-h-screen'>
+      <div>
+        <Navigation/>
+      </div>
       <div>
         <AppRouter/>
       </div>
--- shopdi-web/src/routes/AppRouter.jsx
+++ shopdi-web/src/routes/AppRouter.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 import LoginForm from '../pages/Auth/LoginForm'
 import HomePage from '../pages/HomePage'
 import { Route, Routes } from 'react-router-dom'
+import OrderDetails from '../components/buyer/OrderDetails'
 
 const AppRouter = () => {
   return (
     <div>
         <Routes>
-            <Route path="/*" element={<HomePage />} />
+            <Route path="/*" element={<OrderDetails />} />
             <Route path="/login" exact element={<LoginForm />} />
         </Routes>
     </div>
