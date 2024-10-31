fbbb3491dd0b20659e702e0ea7c3f87da8a5b19f
BuiDoKhoiNguyen <khoinguyen05122004@gmail.com>
2024-11-01 05:49:01 +0700

    initial rating and review

---
+++ shopdi-api/src/main/java/com/rs/shopdiapi/domain/dto/response/RatingResponse.java
@@ -0,0 +1,16 @@
+package com.rs.shopdiapi.domain.dto.response;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RatingResponse {
+    private double rating;
+}
