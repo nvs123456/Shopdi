328ef9f37e20f2228c2a85be4a3b0f7d6c060a77
thinhuet2022 <thinhvnu2022@gmail.com>
2024-11-06 19:43:22 +0700

    add seller profile

--- shopdi-web/src/pages/seller/HomePage.jsx
+++ shopdi-web/src/pages/seller/HomePage.jsx
@@ -5,7 +5,7 @@ import AddProduct from "@/pages/seller/product/AddProduct";
 import ProductDetail from "@/pages/seller/product/ProductDetail";
 import DashBoard from "@/pages/seller/dashboard/DashBoard";
 import OrderList from "./order/OrderList.jsx";
-import SellerFooter from "../../components/Seller/Footer/SellerFooter.jsx";
+import SellerProfile from "./profile/SellerProfile.jsx";
 
 export default function HomePage() {
     return (
@@ -16,7 +16,7 @@ export default function HomePage() {
                 <Route path="products/add-product" exact element={<AddProduct/>}/>
                 <Route path="/products/product-detail" exact element={<ProductDetail/>}/>
                 <Route path="/orders" exact element={<OrderList/>}/>
-                <Route path="/profile" exact element={<>Profile</>}/>
+                <Route path="/profile" exact element={<SellerProfile/>}/>
 
             </Routes>
         </div>
