1d0654d5169f07dea8062fd36506f2d2f0245fc5
thinhuet2022 <thinhvnu2022@gmail.com>
2024-11-21 23:22:43 +0700

    update api order list

--- shopdi-api/src/main/java/com/rs/shopdiapi/mapper/OrderMapper.java
+++ shopdi-api/src/main/java/com/rs/shopdiapi/mapper/OrderMapper.java
@@ -5,23 +5,24 @@ import com.rs.shopdiapi.domain.dto.response.OrderResponse;
 import com.rs.shopdiapi.domain.dto.response.ProductResponse;
 import com.rs.shopdiapi.domain.entity.Order;
 import com.rs.shopdiapi.domain.entity.OrderItem;
-import com.rs.shopdiapi.domain.entity.Product;
-
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 
-@Mapper(componentModel = "spring", uses = {ProductMapper.class})
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring")
 public interface OrderMapper {
-    //@Mapping(target = "billingAddress", source = "")
-    @Mapping(target = "shippingAddress", source = "shippingAddress")
+
+    @Mapping(target = "date", source = "deliveryDate")
     @Mapping(target = "orderId", source = "id")
-    @Mapping(target = "status", source = "orderStatus")
-    @Mapping(target = "date", source = "createdAt")
-    @Mapping(target = "total", source = "totalPrice")
     @Mapping(target = "productCount", expression = "java(order.getOrderItems() != null ? order.getOrderItems().size() : 0)")
- //   @Mapping(target = "products", expression = "java(order.getOrderItems().stream().map(item -> productMapper.toProductResponse(item.getProduct())).toList())")
+    @Mapping(target = "status", source = "orderStatus")
+    @Mapping(target = "total", source = "totalPrice")
+    @Mapping(target = "shippingAddress", source = "shippingAddress")
     OrderResponse toOrderResponse(Order order);
 
     Order toOrder(CreateOrderRequest request);
+
 }
