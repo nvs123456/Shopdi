feaae1aca73e6ed54316542d1c0dc04bc6d85df2
TranVuDucHuy <22021111@vnu.edu.vn>
2024-12-08 11:42:15 +0700

    Can chinh Checkout

--- shopdi-web/src/components/Buyer/Checkout/AddressSelection.jsx
+++ shopdi-web/src/components/Buyer/Checkout/AddressSelection.jsx
@@ -18,18 +18,18 @@ export default function AddressSelection({ onClose, addresses, setAllAddress, cu
         }
     )
     return (
-        <div className="flex flex-col justify-between w-[800px] h-[500px] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white z-10 p-4">
+        <div className="flex flex-col justify-between w-[800px] h-[500px] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white z-10 m-4 rounded">
 
             {
                 isAdding ?
                     (
                         <>
-                            <div className="text-xl text-red font-bold border-b-2 border-gray-400 pb-4">Địa chỉ của tôi</div>
+                            <div className="text-2xl text-yaleBlue font-bold border-b-[1px] border-gray-400 p-4">My Address</div>
                             <AddAddress newAddress={newAddress} setNewAddress={setNewAddress} />
                             <div className="flex flex-row justify-end">
                                 <button onClick={() => {
                                     setIsAdding(false)
-                                }} className="bg-white cursor-pointer border-2 border-gray-400 text-black w-20 p-2 mt-2 mb-2 mr-2">Hủy</button>
+                                }} className="bg-[#FA8232] rounded-sm font-sans hover:bg-orangeRed cursor-pointer text-white w-24 p-2 px-4 mb-8 mr-5">Cancel</button>
                                 <button onClick={() => {
                                     POST(`address/shipping`, newAddress).then((data) => {
                                         if (data.code === "OK") {
@@ -40,19 +40,19 @@ export default function AddressSelection({ onClose, addresses, setAllAddress, cu
                                     // setAllAddress([...addresses, newAddress]);
                                     // console.log(addresses)
                                     setIsAdding(false)
-                                }} className="bg-pumpkin cursor-pointer border-2 border-gray-400 text-white w-20 p-2 mt-2 mb-2 mr-4">Lưu</button>
+                                }} className="bg-[#FA8232] rounded-sm font-sans hover:bg-orangeRed cursor-pointer text-white w-24 p-2 px-4 mb-8 mr-6">Confirm</button>
                             </div>
                         </>
                     ) : (<>
-                        <div className="text-xl text-red font-bold border-b-2 border-gray-400 pb-4">Địa chỉ của tôi</div>
-                        <div className="no-scrollbar overflow-y-scroll h-full"> 
+                        <div className="text-2xl text-yaleBlue font-bold border-b-[1px] border-gray-400 p-4">My Address</div>
+                        <div className="no-scrollbar overflow-y-scroll h-full mx-6">
                             {addresses.map((item) => <Address key={item.addressId} address={item} currentAddress={currentAddress} setCurrentAddress={setCurrentAddress} />)}
 
                         </div>
                         <div className="flex flex-row justify-end ">
-                            <button onClick={() => setIsAdding(true)} className="bg-white cursor-pointer border-2 border-gray-400  text-black h-12 p-2 mt-2 mb-2 mr-2">Thêm địa chỉ mới</button>
-                            <button onClick={() => onClose()} className="bg-white cursor-pointer border-2 border-gray-400 text-black w-20 p-2 mt-2 mb-2 mr-2">Hủy</button>
-                            <button onClick={() => onClose()} className="bg-pumpkin cursor-pointer border-2 border-gray-400 text-white w-20 p-2 mt-2 mb-2 mr-4">Lưu</button>
+                            <button onClick={() => setIsAdding(true)} className="bg-[#FA8232] rounded-sm font-sans hover:bg-orangeRed cursor-pointer text-white h-12 p-2 px-4 my-4 mr-5">Add New Address</button>
+                            <button onClick={() => onClose()} className="bg-[#FA8232] rounded-sm font-sans hover:bg-orangeRed cursor-pointer text-white w-24 p-2 px-4 my-4 mr-5">Cancel</button>
+                            <button onClick={() => onClose()} className="bg-[#FA8232] rounded-sm font-sans hover:bg-orangeRed cursor-pointer text-white w-24 p-2 px-4 my-4 mr-6">Confirm</button>
                         </div>
                     </>)
             }
@@ -61,14 +61,14 @@ export default function AddressSelection({ onClose, addresses, setAllAddress, cu
 }
 function Address({ address, currentAddress, setCurrentAddress }) {
     return (
-        <div className="flex flex-row gap-2 py-2 border-b-2 border-gray-400">
-            <div className="">
-                <input id={`address${address.addressId}`} type="radio" name="selected-address" onClick={() => setCurrentAddress(address)} className="mt-2" />
+        <div className="flex flex-row gap-4 py-4 border-b-2 border-[#E4E7E9]">
+            <div className="flex justify-center items-center">
+                <input id={`address${address.addressId}`} type="radio" name="selected-address" onClick={() => setCurrentAddress(address)} />
             </div>
             <div>
                 <div htmlFor={`address${address.addressId}`} className="flex flex-row">
-                    <div className="pr-2 border-r-2 border-gray-400">{address.firstName} {address.lastName}</div>
-                    <div className="text-gray-600">(SDT:){address.phone}</div>
+                    <div className="pr-2 font-bold">{address.firstName} {address.lastName}</div>
+                    <div className="text-gray-600">(Phone number:){address.phone}</div>
                 </div>
                 <div className="text-gray-600">{address.address}</div>
             </div>
@@ -80,7 +80,7 @@ function AddAddress({newAddress, setNewAddress}) {
 
     return (
         <div className="w-full px-0 md:px-4 mb-4">
-            <h3 className=" text-[20px] text-xl mb-4 border-b-4">Thêm địa chỉ mới</h3>
+            <h3 className=" text-xl mb-4 pt-2 font-bold">Add New Address</h3>
             <div className="grid grid-cols-4 gap-4">
                 {[
                     { label: "firstName", placeholder: "First Name" },
--- shopdi-web/src/components/Buyer/Checkout/Payment.jsx
+++ shopdi-web/src/components/Buyer/Checkout/Payment.jsx
@@ -4,20 +4,20 @@ import { useState } from 'react';
 export default function Payment() {
     const [payment, setPayment] = useState("cod");
     return (
-        <div className="flex flex-col gap-4 p-8 border-2 border-gray-600 mr-10 ml-10 mb-8">
+        <div className="flex flex-col gap-4 px-8 pt-5 pb-8 border-2 border-[#E4E7E9] mx-10 mb-8">
             <div>
-                <label className="text-xl">Payment</label>
-                <div className="flex flex-row justify-center">
+                <label className="text-xl font-bold">Payment Method</label>
+                <div className="flex flex-row justify-center mt-6">
                     <div className="flex flex-row">
                         
-                        <div className="flex flex-col items-center border-r-2 border-gray-400">
+                        <div className="flex flex-col items-center border-r-2 border-[#E4E7E9] pr-10">
                             <label className="text-pumpkin"><AttachMoneyIcon /></label>
-                            <label className="p-2" htmlFor="cod">Thanh toán khi nhận hàng</label>
+                            <label className="p-2" htmlFor="cod">Cash on Delivery</label>
                             <input type="radio" name="payment" id="cod" defaultChecked onChange={()=>{setPayment("cod")}}></input>
                         </div>
-                        <div className="flex flex-col items-center">
+                        <div className="flex flex-col items-center ml-10">
                             <label><img className=" h-[24px]" src='https://stcd02206177151.cloud.edgevnpay.vn/assets/images/logo-icon/logo-primary.svg' /></label>
-                            <label className="p-2" htmlFor="vnpay">Thanh toán bằng VNPay</label>
+                            <label className="p-2" htmlFor="vnpay">VNPay</label>
                             <input type="radio" name="payment" id="vnpay" onChange={()=>{setPayment("vnpay")}}></input>
                         </div>
                     </div>
@@ -25,21 +25,21 @@ export default function Payment() {
             </div>
             {payment == "vnpay" && <div>
                 <div>
-                    <label className="text-xl" htmlFor="name_on_card">Name on card</label>
-                    <input className="w-full h-10 rounded border-2 border-gray-400 outline-none" type="text" id="name_on_card"></input>
+                    <label htmlFor="name_on_card">Name on card</label>
+                    <input className="my-2 w-full h-10 rounded border-2 border-[#E4E7E9] outline-none" type="text" id="name_on_card"></input>
                 </div>
                 <div>
-                    <label className="text-xl" htmlFor="card_number">Card number</label>
-                    <input className="w-full h-10 rounded border-2 border-gray-400 outline-none" type="number" id="card_number"></input>
+                    <label htmlFor="card_number">Card number</label>
+                    <input className="my-2 w-full h-10 rounded border-2 border-[#E4E7E9] outline-none" type="number" id="card_number"></input>
                 </div>
                 <div className="flex flex-row gap-4">
                     <div className="flex flex-col w-1/2">
                         <label htmlFor="exp_month">Expire date</label>
-                        <input className=" h-10 rounded border-2 border-gray-400 outline-none" type="text" id="exp_month" />
+                        <input className=" my-2 h-10 rounded border-2 border-[#E4E7E9] outline-none" type="text" id="exp_month" />
                     </div>
                     <div className="flex flex-col w-1/2">
                         <label htmlFor="cvc">CVC</label>
-                        <input className="h-10 rounded border-2 border-gray-400 outline-none" type="text" id="cvc" />
+                        <input className="my-2 h-10 rounded border-2 border-[#E4E7E9] outline-none" type="text" id="cvc" />
                     </div>
                 </div>
             </div>
