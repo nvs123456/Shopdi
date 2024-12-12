f1975531fe8c0fa07a860dc598b708083a8515aa
nvs123456 <nvs882004@gmail.com>
2024-12-13 01:29:22 +0700

    fix address profile

--- shopdi-web/src/components/Buyer/Checkout/AddressSelection.jsx
+++ shopdi-web/src/components/Buyer/Checkout/AddressSelection.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react"
 import { useState } from "react"
+import SpinnerLoading from "@/components/SpinnerLoading/SpinnerLoading"
 import { POST } from "../../../api/GET"
 export default function AddressSelection({ onClose, addresses, setAllAddress, currentAddress, setCurrentAddress }) {
     const [isAdding, setIsAdding] = useState(false)
@@ -84,184 +85,194 @@ function Address({ address, currentAddress, setCurrentAddress }) {
         </div>
     )
 }
-function AddAddress({ newAddress, setNewAddress }) {
+export function AddAddress({ newAddress, setNewAddress, oldAddress }) {
+    const [loading, setLoading] = useState(false)
     const get = async (path) => {
         return fetch(path, {
             method: "GET"
         })
             .then(res => res.json())
     }
-    const [provinces, setProvinces] = useState([{ province_id: "", province_name: "Province/City" }])
-    const [districts, setDistricts] = useState([{ district_id: "", district_name: "District" }])
-    const [wards, setWards] = useState([{ ward_id: "", ward_name: "Ward" }])
+    const [provinces, setProvinces] = useState([{ province_id: "", province_name: oldAddress !== null ? oldAddress.country : "Province/City" }])
+    const [districts, setDistricts] = useState([{ district_id: "", district_name: oldAddress !== null ? oldAddress.state : "District" }])
+    const [wards, setWards] = useState([{ ward_id: "", ward_name: oldAddress !== null ? oldAddress.city : "Ward" }])
     const [currentProvince, setCurrentProvince] = useState("Province/City")
     const [currentDistrict, setCurrentDistrict] = useState("District")
     const [currentWard, setCurrentWard] = useState("")
     useEffect(() => {
+        // setLoading(true)
         get("https://vapi.vnappmob.com/api/province/").then((data) => {
-            data.results.unshift({ province_id: "", province_name: "Province/City" })
+            if (oldAddress !== null) {
+                data.results.unshift({ province_id: oldAddress.country, province_name: oldAddress.country })
+            } else {
+                data.results.unshift({ province_id: "", province_name: "Province/City" })
+            }
             setProvinces(data.results)
+            // setLoading(false)
         })
-    },[])
+    }, [])
     useEffect(() => {
-        if(currentProvince.province_name !== "Province/City")
-        get(`https://vapi.vnappmob.com/api/province/district/${currentProvince.province_id}`).then((data) => {
-            data.results.unshift({ district_id: "", district_name: "District" })
-            setDistricts(data.results)
-        })
-    },[currentProvince])
+        // setLoading(true)
+        if (currentProvince.province_name !== "Province/City")
+            get(`https://vapi.vnappmob.com/api/province/district/${currentProvince.province_id}`).then((data) => {
+                if (oldAddress !== null) {
+                    data.results.unshift({ district_id: oldAddress.state, district_name: oldAddress.state })
+                } else {
+                    data.results.unshift({ district_id: "", district_name: "District" })
+                }
+                setDistricts(data.results)
+                setLoading(false)
+            })
+        // else {
+        //     setLoading(false)
+        // }
+    }, [currentProvince])
     useEffect(() => {
-        if(currentDistrict.district_name !== "District")
-        get(`https://vapi.vnappmob.com/api/province/ward/${currentDistrict.district_id}`).then((data) => {
-            data.results.unshift({ ward_id: "", ward_name: "Ward" })
-            setWards(data.results)
-        })
-    },[currentDistrict])
+        // setLoading(true)
+        if (currentDistrict.district_name !== "District")
+            get(`https://vapi.vnappmob.com/api/province/ward/${currentDistrict.district_id}`).then((data) => {
+                if (oldAddress !== null) {
+                    data.results.unshift({ ward_id: oldAddress.city, ward_name: oldAddress.city })
+                } else {
+                    data.results.unshift({ ward_id: "", ward_name: "Ward" })
+                }
+                setWards(data.results)
+                setLoading(false)
+            })
+        // else {
+        //     setLoading(false)
+        // }
+    }, [currentDistrict])
 
-    return (
-        <div className="w-full px-0 md:px-4 mb-4">
-            <h3 className=" text-xl mb-4 pt-2 font-bold">Add New Address</h3>
-            <div className="grid grid-cols-4 gap-4">
-                {/* {[
-                    { label: "firstName", name: "firstName", placeholder: "First Name" },
-                    { label: "lastName", name: "lastName", placeholder: "Last Name" },
-                    // { label: "companyName", name: "companyName", placeholder: "Optional", colSpan: true },
-                    { label: "address", name: "address", placeholder: "Street address", colSpan: true },
-                    { label: "Province/City", name: "country", placeholder: "Province/City" },
-                    { label: "District", name: "state", placeholder: "District" },
-                    { label: "Commune/Ward", name: "city", placeholder: "Commune/Ward" },
-                    // { label: "zip Code", name: "zipCode", placeholder: "Zip Code" },
-                    { label: "Email", name: "email", placeholder: "email@example.com", colSpan: true },
-                    { label: "Phone Number", name: "phoneNumber", placeholder: "Phone number", colSpan: true },
-                ].map((field, index) => (
-                    <div key={index} className={field.colSpan ? "col-span-2" : ""}>
+    if (!loading) {
+        return (
+            <div className="w-full px-0 md:px-4 mb-4">
+                <h3 className=" text-xl mb-4 pt-2 font-bold">Add New Address</h3>
+                <div className="grid grid-cols-4 gap-4">
+                    <div className="col-span-2">
                         <label className="block text-[15px] md:text-sm mb-1">
-                            {field.label}
+                            First name
+                        </label>
+                        <input
+                            defaultValue={oldAddress ? oldAddress.firstName : ""}
+                            type="text"
+                            className="w-full border-[#E4E7E9] border-2 rounded p-2"
+                            placeholder="First name"
+                            onChange={(e) => {
+                                setNewAddress({ ...newAddress, firstName: e.target.value })
+                            }}
+                        />
+                    </div>
+                    <div className="col-span-2">
+                        <label className="block text-[15px] md:text-sm mb-1">
+                            Last name
+                        </label>
+                        <input
+                            defaultValue={oldAddress ? oldAddress.lastName : ""}
+                            type="text"
+                            className="w-full border-[#E4E7E9] border-2 rounded p-2"
+                            placeholder="Last name"
+                            onChange={(e) => {
+                                setNewAddress({ ...newAddress, lastName: e.target.value })
+                            }}
+                        />
+                    </div>
+                    <div className="col-span-2">
+                        <label className="block text-[15px] md:text-sm mb-1">
+                            Email
+                        </label>
+                        <input
+                            defaultValue={oldAddress ? oldAddress.email : ""}
+                            type="text"
+                            className="w-full border-[#E4E7E9] border-2 rounded p-2"
+                            placeholder="example@gmail.com"
+                            onChange={(e) => {
+                                setNewAddress({ ...newAddress, email: e.target.value })
+                            }}
+                        />
+                    </div>
+                    <div className="col-span-2">
+                        <label className="block text-[15px] md:text-sm mb-1">
+                            Phone number
+                        </label>
+                        <input
+                            defaultValue={oldAddress ? oldAddress.phoneNumber : ""}
+                            type="text"
+                            className="w-full border-[#E4E7E9] border-2 rounded p-2"
+                            placeholder="Phone number"
+                            onChange={(e) => {
+                                setNewAddress({ ...newAddress, phoneNumber: e.target.value })
+                            }}
+                        />
+                    </div>
+                    <div className="col-span-2">
+                        <label className="block text-[15px] md:text-sm mb-1">
+                            Province/City
+                        </label>
+                        <select className="w-full border-[#E4E7E9] border-2 rounded p-2 bg-white" onChange={(e) => {
+                            setNewAddress({ ...newAddress, country: e.target.value })
+                            setCurrentProvince(provinces.find(province => province.province_name === e.target.value))
+                        }}>
+                            {
+                                provinces.map((province, index) => (
+                                    <option value={province.province_name} key={`province${index}`}>{province.province_name}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    <div className="col-span-2">
+                        <label className="block text-[15px] md:text-sm mb-1">
+                            District
+                        </label>
+                        <select className="w-full border-[#E4E7E9] border-2 rounded p-2 bg-white" onChange={(e) => {
+                            setNewAddress({ ...newAddress, state: e.target.value })
+                            setCurrentDistrict(districts.find(district => district.district_name === e.target.value))
+                        }}>
+                            {
+                                districts.map((district, index) => (
+                                    <option value={district.district_name} key={`district${index}`}>{district.district_name}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+
+                    <div className="col-span-2">
+                        <label className="block text-[15px] md:text-sm mb-1">
+                            Commune/Ward
+                        </label>
+                        <select className="w-full border-[#E4E7E9] border-2 rounded p-2 bg-white" onChange={(e) => {
+                            setNewAddress({ ...newAddress, city: e.target.value })
+                            setCurrentWard(wards.find(ward => ward.ward_name === e.target.value))
+                        }}>
+                            {
+                                wards.map((ward, index) => (
+                                    <option value={ward.ward_name} key={`ward${index}`}>{ward.ward_name}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    <div className="col-span-2">
+                        <label className="block text-[15px] md:text-sm mb-1">
+                            Address
                         </label>
                         <input
                             type="text"
                             className="w-full border-[#E4E7E9] border-2 rounded p-2"
-                            placeholder={field.placeholder}
+                            placeholder="House number, street, ..."
                             onChange={(e) => {
-                                setNewAddress({ ...newAddress, [field.name]: e.target.value })
-                                // console.log(newAddress)
+                                setNewAddress({ ...newAddress, address: e.target.value })
                             }}
                         />
                     </div>
-                ))} */}
-                <div className="col-span-2">
-                    <label className="block text-[15px] md:text-sm mb-1">
-                        First name
-                    </label>
-                    <input
-                        type="text"
-                        className="w-full border-[#E4E7E9] border-2 rounded p-2"
-                        placeholder="First name"
-                        onChange={(e) => {
-                            setNewAddress({ ...newAddress, firstName: e.target.value })
-                        }}
-                    />
                 </div>
-                <div className="col-span-2">
-                    <label className="block text-[15px] md:text-sm mb-1">
-                        Last name
-                    </label>
-                    <input
-                        type="text"
-                        className="w-full border-[#E4E7E9] border-2 rounded p-2"
-                        placeholder="Last name"
-                        onChange={(e) => {
-                            setNewAddress({ ...newAddress, lastName: e.target.value })
-                        }}
-                    />
-                </div>
-                <div className="col-span-2">
-                    <label className="block text-[15px] md:text-sm mb-1">
-                        Email
-                    </label>
-                    <input
-                        type="text"
-                        className="w-full border-[#E4E7E9] border-2 rounded p-2"
-                        placeholder="example@gmail.com"
-                        onChange={(e) => {
-                            setNewAddress({ ...newAddress, email: e.target.value })
-                        }}
-                    />
-                </div>
-                <div className="col-span-2">
-                    <label className="block text-[15px] md:text-sm mb-1">
-                        Phone number
-                    </label>
-                    <input
-                        type="text"
-                        className="w-full border-[#E4E7E9] border-2 rounded p-2"
-                        placeholder="Phone number"
-                        onChange={(e) => {
-                            setNewAddress({ ...newAddress, phoneNumber: e.target.value })
-                        }}
-                    />
-                </div>
-                <div className="col-span-2">
-                    <label className="block text-[15px] md:text-sm mb-1">
-                        Province/City
-                    </label>
-                    <select className="w-full border-[#E4E7E9] border-2 rounded p-2 bg-white" onChange={(e) => { 
-                        setNewAddress({ ...newAddress, country: e.target.value }) 
-                        setCurrentProvince(provinces.find(province => province.province_name === e.target.value))
-                        }}>
-                        {
-                            provinces.map((province, index) => (
-                                <option  value={province.province_name} key={`province${index}`}>{province.province_name}</option>
-                            ))
-                        }
-                    </select>
-                </div>
-                <div className="col-span-2">
-                    <label className="block text-[15px] md:text-sm mb-1">
-                        District
-                    </label>
-                    <select className="w-full border-[#E4E7E9] border-2 rounded p-2 bg-white" onChange={(e) => { 
-                        setNewAddress({ ...newAddress, state: e.target.value }) 
-                        setCurrentDistrict(districts.find(district => district.district_name === e.target.value))
-                        }}>
-                        {
-                            districts.map((district, index) => (
-                                <option  value={district.district_name} key={`district${index}`}>{district.district_name}</option>
-                            ))
-                        }
-                    </select>
-                </div>
-                
-                <div className="col-span-2">
-                    <label className="block text-[15px] md:text-sm mb-1">
-                        Commune/Ward
-                    </label>
-                    <select className="w-full border-[#E4E7E9] border-2 rounded p-2 bg-white" onChange={(e) => { 
-                        setNewAddress({ ...newAddress, city: e.target.value }) 
-                        setCurrentWard(wards.find(ward => ward.ward_name === e.target.value))
-                        }}>
-                        {
-                            wards.map((ward, index) => (
-                                <option  value={ward.ward_name} key={`ward${index}`}>{ward.ward_name}</option>
-                            ))
-                        }
-                    </select>
-                </div>
-                <div className="col-span-2">
-                    <label className="block text-[15px] md:text-sm mb-1">
-                        Address
-                    </label>
-                    <input
-                        type="text"
-                        className="w-full border-[#E4E7E9] border-2 rounded p-2"
-                        placeholder="House number, street, ..."
-                        onChange={(e) => {
-                            setNewAddress({ ...newAddress, address: e.target.value })
-                        }}
-                    />
-                </div>
-            </div>
 
-        </div>
-    );
+            </div>
+        );
+    } else {
+        return (
+            <div className="w-full px-0 md:px-4 mb-4">
+                <SpinnerLoading />
+            </div >
+        )
+    }
 }
