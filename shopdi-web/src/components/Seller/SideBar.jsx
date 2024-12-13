20be442c61f8dee0d20f699a5f78c47da1823b06
TranVuDucHuy <22021111@vnu.edu.vn>
2024-12-13 14:35:32 +0700

    Update dashboard

--- shopdi-web/src/components/Seller/SideBar.jsx
+++ shopdi-web/src/components/Seller/SideBar.jsx
@@ -6,33 +6,33 @@ import LocalMallIcon from '@mui/icons-material/LocalMall';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 export default function SideBar() {
     return (
-        <div className="w-[15%] h-full-screen bg-yaleBlue gap-4 font-sans flex flex-col">
+        <div className="w-[15%] h-full-screen bg-yaleBlue gap-7 font-sans flex flex-col">
 
 
-            <div className="w-full flex flex-col items-center">
-                <img src={shopdiLogo} className="w-1/2" alt="" />
+            <div className="w-full flex flex-col items-center mt-6">
+                <img src={shopdiLogo} className="w-3/5" alt="" />
             </div>
             <Link to="">
-                <div className="text-gray-400 hover:text-white text-sm ml-1">
-                    <DashboardIcon className="mr-1" />
-                    Trang chủ
+                <div className="text-gray-300 hover:text-white text-[18px] ml-6 font-semibold">
+                    <DashboardIcon className=" mr-2 mb-1" />
+                    Dashboard
                 </div>
             </Link>
             <Link to="products">
-                <div className="text-gray-400 hover:text-white  text-sm ml-1">
-                    <CategoryIcon className="mr-1" />
-                    Quản lý sản phẩm
+                <div className="text-gray-300 hover:text-white text-[18px] ml-6 font-semibold">
+                    <CategoryIcon className="mr-2 mb-1" />
+                    Products
                 </div>
             </Link>
             <Link to="orders">
-                <div className="text-gray-400 hover:text-white  text-sm ml-1">
-                    <LocalMallIcon className="mr-1" />
+                <div className="text-gray-300 hover:text-white text-[18px] ml-6 font-semibold">
+                    <LocalMallIcon className="mr-2 mb-1" />
                     Orders
                 </div>
             </Link>
             <Link to="profile">
-                <div className="text-gray-400 hover:text-white text-sm ml-1">
-                    <AccountCircleIcon className="mr-1" />
+                <div className="text-gray-300 hover:text-white text-[18px] ml-6 font-semibold">
+                    <AccountCircleIcon className="mr-2 mb-1" />
                     Profile
                 </div>
             </Link>
